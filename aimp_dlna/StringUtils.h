cbbf340272c933d5b926063fbddc3dd957f402a3
Unknown <kamil.pietka@yahoo.pl>
2018-09-08 03:15:17 +0200

    Add additional string conversion methods

--- aimp_dlna/StringUtils.h
+++ aimp_dlna/StringUtils.h
@@ -7,12 +7,15 @@ private:
 	static wstring_convert<codecvt_utf8<wchar_t>, wchar_t> converter;
 
 public:
+	static inline wstring ToWideString(const wchar_t* s) { return wstring(s); }
 	static inline wstring ToWideString(const char* s) {	return converter.from_bytes(s);	}
 	static inline wstring ToWideString(const string& s) { return converter.from_bytes(s); }
 	static inline wstring ToWideString(const NPT_String& s) { return ToWideString(s.GetChars()); }
+	static inline wstring ToWideString(IAIMPString& s) { return ToWideString(s.GetData()); }
 
 	static inline string ToString(const wchar_t* s) { return converter.to_bytes(s); }
 	static inline string ToString(const wstring& s) { return converter.to_bytes(s); }
+	static inline string ToString(IAIMPString& s) { return ToString(s.GetData()); }
 
 	static wchar_t* ToWideCharArray(const NPT_String& s) {
 		auto length = s.GetLength();
