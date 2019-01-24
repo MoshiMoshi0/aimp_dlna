f44e74370a1ade3fd748b05f42dde3574c99a64c
Unknown <kamil.pietka@yahoo.pl>
2019-01-24 12:06:38 +0100

    Fix build

--- aimp_dlna/AimpDlnaAlbumArtProvider.h
+++ aimp_dlna/AimpDlnaAlbumArtProvider.h
@@ -2,11 +2,15 @@
 
 class AimpDlnaAlbumArtProvider : public IUnknownInterfaceImpl<IAIMPMLAlbumArtProvider, IID_IAIMPMLAlbumArtProvider> {
 private:
-	PLT_SyncMediaBrowser* mediaBrowser;
+	shared_ptr<PLT_SyncMediaBrowser> mediaBrowser;
 
 public:
-	AimpDlnaAlbumArtProvider(PLT_SyncMediaBrowser* mediaBrowser) {
+	AimpDlnaAlbumArtProvider(shared_ptr<PLT_SyncMediaBrowser> mediaBrowser) {
 		this->mediaBrowser = mediaBrowser;
+	}
+
+	~AimpDlnaAlbumArtProvider() {
+		mediaBrowser = nullptr;
 	}
 
 	HRESULT WINAPI Get(IAIMPObjectList* Fields, VARIANT* Values, IAIMPPropertyList* Options, IAIMPImageContainer** Image);
